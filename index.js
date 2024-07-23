a572629505ab3a8ca2c51c8637f98d52edb27753
Ilias Seddik <iseddik@telequebec.tv>
2024-07-23 10:52:32 -0700

    deinterlaced before description

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.post("/video", async (req,res) => {
 	let duration = parseInt(rawDuration.toString());
 	const frameNumber = Math.round(Math.log(duration)) * 4;
 
-	let command0 = `ffmpeg -i ${basePath}${filePath} -vf bwdif=mode=send_field:parity=auto:deint=all -c:v mpeg2video -b:v 50M -minrate 50M -maxrate 50M -bufsize 2M -pix_fmt yuv422p -c:a copy ${folder}/${name}_deinterlaced.mxf`;
+	/*let command0 = `ffmpeg -i ${basePath}${filePath} -vf bwdif=mode=send_field:parity=auto:deint=all -c:v mpeg2video -b:v 50M -minrate 50M -maxrate 50M -bufsize 2M -pix_fmt yuv422p -c:a copy ${folder}/${name}_deinterlaced.mxf`;
 
 
 	execSync(command0, (err, stdout, stderr) => {
@@ -54,10 +54,10 @@ app.post("/video", async (req,res) => {
 			res.status(500).send("Error deinterlacing the video");
 		}
 		console.log("deinterlacing done successfully")
-	})
+	}) */
 
 	let step = Math.round(Math.log(duration));
- 	let command = `ffmpeg -i ${folder}/${name}_deinterlaced.mxf -vf "thumbnail" -r 1/${step} -q:v 4 -vframes ${frameNumber} ${folder}/frame-%02d.jpg`;
+ 	let command = `ffmpeg -i ${basePath}${filePath} -vf "thumbnail" -r 1/${step} -q:v 4 -vframes ${frameNumber} ${folder}/frame-%02d.jpg`;
 	let command2 = `ffmpeg -i ${basePath}${filePath} -i ${folder}/frame-01.jpg -map 1 -map 0 -c copy -disposition:0 attached_pic -y ${folder}/${name}_thumb.mxf`;
 
 	exec(command, (err, stdout, stderr) => {
@@ -69,7 +69,10 @@ app.post("/video", async (req,res) => {
 		const fs = require('fs');
 		let framesBase64 = [];
 		for (let i = 1; i <= frameNumber; i++) {
-			let base64 = fs.readFileSync(`${folder}/frame-${i > 9 ? i : "0"+i}.jpg`, { encoding: 'base64' });
+			//deinterlace the frame image with yadif
+			execSync(`ffmpeg -i ${folder}/frame-${i > 9 ? i : "0"+i}.jpg -vf yadif=mode=send_frame ${folder}/frame-${i > 9 ? i : "0"+i}d.jpg`);
+						
+			let base64 = fs.readFileSync(`${folder}/frame-${i > 9 ? i : "0"+i}d.jpg`, { encoding: 'base64' });
 			framesBase64.push({
 				type: "image_url", 
 				image_url: { url: `data:image/jpeg;base64,${base64}`, detail: "low" }
@@ -141,12 +144,12 @@ app.post("/video", async (req,res) => {
 						}
 					]
 				}
-				fs.unlink(`${folder}/${name}_deinterlaced.mxf`, (err)=>{
+				/*fs.unlink(`${folder}/${name}_deinterlaced.mxf`, (err)=>{
 					if(err){
 						return console.log("failed to remove the altered video")
 					}
 					console.log("Altered video deleted sucessfully")
-				})		
+				}) */		
 				fetch('https://api.openai.com/v1/chat/completions', {
 					method: 'POST',
 					headers: {
@@ -167,7 +170,7 @@ app.post("/video", async (req,res) => {
 					//recreate the selected frame in a new file in a folder named "selectedFrame" after creating the folder
 					fs.mkdirSync(`${folder}/selectedFrame`);
 					fs.writeFileSync(`${folder}/selectedFrame/selectedFrame.txt`, `La frame seléctionnée est la ${selectedFrame}. \n ${content?.explication}`); 
-					fs.copyFileSync(`${ folder }/frame-${selectedFrame > 9 ? selectedFrame : "0"+selectedFrame}.jpg`, `${ folder }/selectedFrame/frame-${selectedFrame > 9 ? selectedFrame : "0"+selectedFrame}.jpg`);
+					fs.copyFileSync(`${ folder }/frame-${selectedFrame > 9 ? selectedFrame : "0"+selectedFrame}d.jpg`, `${ folder }/selectedFrame/frame-${selectedFrame > 9 ? selectedFrame : "0"+selectedFrame}.jpg`);
 					res.status(200).send(data);
 				}).catch(err => {
 					console.error("error = ",err);
@@ -179,12 +182,12 @@ app.post("/video", async (req,res) => {
 				res.status(500).send("Error with GPT-4o in the code");
 			}
 		}).catch(err => {
-			fs.unlink(`${folder}/${name}_deinterlaced.mxf`, (err)=>{
+			/*fs.unlink(`${folder}/${name}_deinterlaced.mxf`, (err)=>{
 				if(err){
 					return console.log("failed to remove the altered video")
 				}
 				console.log("Altered video deleted sucessfully")
-			})
+			})*/
 			console.error(err);
 			res.status(500).send("Error with GPT-4o in first request");
 		});
